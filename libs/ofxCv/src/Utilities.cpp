450d60c625640d4aa3b471b13b3d18e9a5b8c08f
nkint <nootropic.registrazioni@gmail.com>
2012-03-19 18:16:02 +0100

    try to fix Utils.cpp

--- libs/ofxCv/src/Utilities.cpp
+++ libs/ofxCv/src/Utilities.cpp
@@ -63,16 +63,7 @@ namespace ofxCv {
 		return polyline;
 	}
 
-    vector<ofPoint> toOf(const vector<cv::Point>& src) {
-        vector<ofPoint> ret(src.size());
-        cv::Point p;
-        for(int i=0; i!=src.size();++i) {
-            p = src.at(i);
-            ret.push_back(ofPoint(p.x, p.y));
-        }
-
-        return ret;
-    }
+    
 	
 	float getMaxVal(int depth) {
 		switch(depth) {
